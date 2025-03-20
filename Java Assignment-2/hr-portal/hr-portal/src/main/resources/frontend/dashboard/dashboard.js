ebd358c11cefa6ce91d457196643df04159fdae4
Arun Parihar <arunsinghparihar3219@gmail.com>
2025-03-20 13:20:39 +0530

    dashboard completed

---
+++ Java Assignment-2/hr-portal/hr-portal/src/main/resources/frontend/dashboard/dashboard.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', fetchEmployees);
+
+
+async function fetchEmployees(){
+    try{
+        const response = await fetch('http://localhost:8080/api/employees');
+        if(!response.ok){
+            throw new Error(`Failed to fetch employees: ${response.status}`);
+        }
+     const employees = await response.json();
+
+     const tableBody = document.getElementById('employeeTableBody');
+     tableBody.innerHTML = '';
+
+     employees.forEach(employee =>{
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${employee.id} </td>
+        <td>${employee.name} </td>
+        <td>${employee.email} </td>
+        <td>${employee.department} </td>
+        <td>${employee.salary} </td>
+        <td>
+        <button class = "action-btn" onClick={editEmployee(${employee.id})}>Edit</button>
+        <button class = "action-btn" onClick={deleteEmployee(${employee.id})}>Delete</button>
+        </td>
+        `;
+        tableBody.appendChild(row);
+     })
+    } catch(error){
+        console.error('Error fetching employees:', error);
+        alert('Error fetching employees');
+    }
+}
+
+// Edit employee
+
+function editEmployee(employeeId){
+    window.location.href = `edit-employee.html?id=${employeeId}`;
+}
+
+// Delete employee
+
+async function deleteEmployee(employeeId){
+    const confirmDelete= confirm('Are you sure you want to delete this employee');
+    
+    if(confirmDelete){
+        try{
+            const response = await fetch(`http://localhost:8080/api/employees/${employeeId}}`,{
+                method: 'DELETE',
+            }); 
+            if (response.ok) {
+                alert("Employee deleted successfully!");
+                fetchEmployees(); // Refresh employee list
+              } else {
+                const errorText = await response.text();
+                alert(`Error: ${errorText}`);
+              }
+        }catch(error){
+            console.error('Error deleting employee:', error);
+            alert('Error deleting employee');
+        }
+    }
+  
+}
--- Java Assignment-2/hr-portal/hr-portal/src/main/resources/frontend/login/login.js
+++ Java Assignment-2/hr-portal/hr-portal/src/main/resources/frontend/login/login.js
@@ -14,7 +14,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
   
       if (response.ok) {
         alert('Login Successful!');
-        window.location.href = '../dashboard.html'; // Redirect to Dashboard
+        window.location.href = '../dashboard/dashboard.html'; // Redirect to Dashboard
       } else {
         const errorMessage = await response.text();
         document.getElementById('error').textContent = errorMessage;
